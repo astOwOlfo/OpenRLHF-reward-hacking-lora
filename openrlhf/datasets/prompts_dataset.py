083a8f3934b14411018b1e16cfd967e62deecbdf
Joey <jyudel@gmail.com>
2025-02-07 15:51:40 -0800

    apps

--- openrlhf/datasets/prompts_dataset.py
+++ openrlhf/datasets/prompts_dataset.py
@@ -62,7 +62,7 @@ class PromptDataset(Dataset):
             if full_data is not None:
                 data_entry["full_data"] = full_data
                 if "input_output" in full_data:
-                    unit_tests_json = list(json.loads(full_data["input_output"]))[:5]
+                    unit_tests_json = list(json.loads(full_data["input_output"]))
                     full_data["input_output"] = json.dumps(unit_tests_json)
             if solution is not None:
                 data_entry["solution"] = solution
